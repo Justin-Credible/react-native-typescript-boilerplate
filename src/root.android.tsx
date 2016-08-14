5bfcc12fb54998ed3e96dfa12e81b6e71883fb7d
Justin Unterreiner <justin.unterreiner@gmail.com>
2016-08-14 15:09:41 -0700

    Fix tsx compilation errors

--- src/root.android.tsx
+++ src/root.android.tsx
@@ -1,18 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
+import * as React from "react";
+import { Component } from "react";
+
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
 
-class SampleApp1 extends Component {
+class SampleApp1 extends Component<any, any> {
   render() {
     return (
       <View style={styles.container}>
@@ -37,17 +34,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
+  } as __React.ViewStyle,
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
+  } as __React.TextStyle,
+
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
+  } as __React.TextStyle,
 });
 
 AppRegistry.registerComponent('SampleApp1', () => SampleApp1);
